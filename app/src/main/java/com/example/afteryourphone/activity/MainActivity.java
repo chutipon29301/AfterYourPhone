eaf4d244024ba88a4f038e4928931c872b7d60a8
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-02-18 02:44:12 +0700

    Fix confloct

--- app/src/main/java/com/example/afteryourphone/activity/MainActivity.java
+++ app/src/main/java/com/example/afteryourphone/activity/MainActivity.java
@@ -2,25 +2,14 @@ package com.example.afteryourphone.activity;
 
 import android.location.Location;
 import android.os.Bundle;
-<<<<<<<HEAD
 import android.support.annotation.NonNull;
-=======
-
-        >>>>>>>75b128482170eaf9a9b38af77ca8d9c8f57dbd5e
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 
 import com.example.afteryourphone.R;
-<<<<<<<HEAD
 import com.example.afteryourphone.dao.PlaceDetailDao;
 import com.example.afteryourphone.dao.PlaceListDetailDao;
-=======
-
-import com.example.afteryourphone.dao.PlaceDetailDao;
-import com.example.afteryourphone.dao.PlaceListDetailDao;
-
->>>>>>>75b128482170eaf9a9b38af77ca8d9c8f57dbd5e
 import com.example.afteryourphone.manager.LocationManager;
 import com.example.afteryourphone.manager.PlaceDetailDataManager;
 import com.example.afteryourphone.manager.PlaceListDataManager;
@@ -42,46 +31,6 @@ public class MainActivity extends AppCompatActivity implements PlaceDetailDataMa
         setContentView(R.layout.activity_main);
         Sensey.getInstance().init(this);
         Sensey.getInstance().startTouchTypeDetection(this, touchTypListener);
-
-<<<<<<<HEAD
-                speakerbox = new Speakerbox(getApplication());
-
-        LocationManager.getInstance().getLocation(this, this, REQUEST_LIST);
-
-//        FusedLocationProviderClient mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-//        mFusedLocationClient.getLastLocation()
-//                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-//                    @Override
-//                    public void onSuccess(Location location) {
-//                        if (location != null) {
-//                            Log.d(TAG, "onSuccess: " + location);
-//                            PlaceListDataManager.getInstance().getPlace(location.getLatitude(), location.getLongitude());
-//                            LocationManager.getInstance().setLocation(location);
-//                        }
-//                    }
-//                });
-//        LocationManager location = LocationManager.getInstance();
-//        speakerbox.play("Current Temperature is" + HttpManager.getInstance().getApiService().getTemp(new LocationDao(location.getlatitude(), location.getlongtitude())));
-=======
-
-        speakerbox = new Speakerbox(getApplication());
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if (location != null) {
-                            Log.d(TAG, "onSuccess: " + location);
-                            PlaceListDataManager.getInstance().getPlace(location.getLatitude(), location.getLongitude());
-                            LocationManager.getInstance().setLocation(location);
-                        }
-                    }
-                });
-        LocationManager location = LocationManager.getInstance();
-        speakerbox.play("Tab long press to listen to the instructions");
-//        speakerbox.play("Current Temperature is"+ HttpManager.getInstance().getApiService().getTemp(new LocationDao(location.getlatitude(),location.getlongtitude())));
->>>>>>>75 b128482170eaf9a9b38af77ca8d9c8f57dbd5e
 
     }
 
@@ -201,24 +150,17 @@ public class MainActivity extends AppCompatActivity implements PlaceDetailDataMa
         @Override
         public void onLongPress() {
             Log.d("gesture", "longpress");
-
-<<<<<<<HEAD
-            speakerbox.play("Here's the instructions. Please swipe your finger on the screen to the left for visit the next place, and" +
-                    "swipe to the right to revisit the previous one. Swipe up to listen to detail of that particular place and tab on the screen to listen again");
-=======
             speakerbox.play("Here's the instructions," + " Please swipe your finger on the screen to the left for visit the next place, and" +
                     "swipe to the right to revisit the previous one, Swipe up to listen to detail of that particular place and tab on the screen to listen again");
->>>>>>>75 b128482170eaf9a9b38af77ca8d9c8f57dbd5e
 
         }
     };
 
     @Override
     public void onLoad(PlaceDetailDao placeDetail) {
-<<<<<<<HEAD
         speakerbox.play(PlaceListDataManager.getInstance().current().getName() +
-                ", Distance " + placeDetail.getDistance() +
-                ", Time " + placeDetail.getTime());
+                ", Distance " + Math.round(placeDetail.getDistance() / 100) / 10.0 + " kilometers" +
+                ", Estimated time " + Math.round(placeDetail.getTime() / 6) / 10.0 + " minutes");
     }
 
     @Override
@@ -241,11 +183,5 @@ public class MainActivity extends AppCompatActivity implements PlaceDetailDataMa
                 PlaceDetailDataManager.getInstance().getPlaceDetail(PlaceListDataManager.getInstance().current().getPlaceId(), location.getLatitude(), location.getLongitude(), this);
                 break;
         }
-=======
-
-        speakerbox.play(PlaceListDataManager.getInstance().current().getName() +
-                ", Distance " + Math.round(placeDetail.getDistance() / 100) / 10.0 + " kilometers" +
-                ", Estimated time " + Math.round(placeDetail.getTime() / 6) / 10.0 + " minutes");
->>>>>>>75 b128482170eaf9a9b38af77ca8d9c8f57dbd5e
     }
 }

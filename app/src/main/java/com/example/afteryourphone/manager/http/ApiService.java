b213f7000a19857ee4a876137d66809bf821a749
Tanyawat <hamzaabamboo@gmail.com>
2018-02-18 04:57:13 +0700

    a rai kor mai ru

--- app/src/main/java/com/example/afteryourphone/manager/http/ApiService.java
+++ app/src/main/java/com/example/afteryourphone/manager/http/ApiService.java
@@ -14,13 +14,13 @@ import retrofit2.http.POST;
  */
 
 public interface ApiService {
-
+    //Nearby location
     @POST("https://prod-18.southeastasia.logic.azure.com/workflows/5e98e1bc5e2a463c86c494ab59e358d9/triggers/manual/paths/invoke/location?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=XJ9Fhh2BmefTg_tufIHxyiJGIfu6n8jLRFv7et822X8")
     Observable<PlaceListDao> getPlaceList(@Body LocationDao locationDao);
-
+    //Travelling distance and time
     @POST("https://prod-03.southeastasia.logic.azure.com/workflows/e331547acebd4bf9a47c7abc09613d05/triggers/manual/paths/invoke/travelTime?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=QidO2UOGKjKRWztJunfcU0I-hRlDbFNgOjlI_Ite3PM")
     Observable<PlaceDetailDao> getPlaceDetail(@Body PlaceDao placeDao);
-
+    //Weather info
     @POST("https://prod-24.southeastasia.logic.azure.com/workflows/9377fa40e5f24c00bc9b4fc84ecbcb1d/triggers/manual/paths/invoke/weather?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=PcxhUj3ErMvdVuG4EXms_3H5ZxxSRbpWzSzJQ_kPPpk")
     Observable<PlaceListDao> getTemp(@Body LocationDao locationDao);
 
